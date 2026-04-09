4a9a544f342864144e1e11c30f437eb69f8452d1
Jakob Schroll <jakobschroll06@gmail.com>
2026-04-08 23:13:15 -0400

    updated getForcast

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ def getAlerts(s: str):
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        response = response.json()
+        data = response.json()
+        if len(data['features']) == 0:
+            print("No warnings")
+            return
 
-        for x in response['features']:
+        for x in data['features']:
 
             print(x["properties"]["event"])
             print(x["properties"]["description"])
-            print("---------------------------------------------")
+            print("-" * 200)
     else:
         print(f"Error: {response.status_code}")
 
@@ -28,16 +31,11 @@ def getForcast(lat, lon):
     if response.status_code == 200:
         data = response.json()
 
-
-        ##### data["properties"]['periods'] will have most of the info I am looking for. #####
-
-        # for x in data["properties"]['periods']:
-        #     print(x)
-
         for i in range(len(data["properties"]['periods'])):
             print(data["properties"]["periods"][i]["name"])
-            print(data["properties"]['periods'][i]["temperature"], data["properties"]['periods'][i]["temperatureUnit"])
+            print(f"Temperature: {data["properties"]['periods'][i]["temperature"]}{data["properties"]['periods'][i]["temperatureUnit"]}")
             print(data["properties"]["periods"][i]["detailedForecast"])
+            print(f"Wind Speed: {data["properties"]["periods"][i]["windSpeed"]}")
             print("-" * 200)
 
     else:
@@ -45,9 +43,10 @@ def getForcast(lat, lon):
 
 if __name__ == '__main__':
 
-    #state = str(input("Input State (e.g. WA): "))
-    #getAlerts(state)
-    # print("-" * 20)
+    # state = str(input("Input State (e.g. WA): "))
+    state = "KY"
+    getAlerts(state)
+    print("#" * 200)
 
     getForcast(50, 78)
 
